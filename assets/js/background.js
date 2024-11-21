4461ae4eddaf1c534ce65a1a0829dd74b343122b
Al-rimi <ggak71@gmail.com>
2024-11-22 07:28:42 +0800

    canvas

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,18 +1,31 @@
 var canvas = document.getElementById('beerCanvas');
 var ctx = canvas.getContext('2d');
 var particles = [];
-var particleCount = 100;
+var particleCount = 140;
 
-for (var i = 0; i < particleCount; i++) {
-    particles.push(new particle());
+function resizeCanvas() {
+    var oldWidth = canvas.width;
+    var oldHeight = canvas.height;
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    particles.forEach((p) => {
+        p.x = (p.x / oldWidth) * canvas.width;
+        p.y = (p.y / oldHeight) * canvas.height;
+    });
 }
 
 function particle() {
     this.x = Math.random() * canvas.width;
-    this.y = canvas.height + Math.random() * 300;
-    this.speed = Math.random()/ 1.8;
-    this.radius = Math.random() * 8;
-    this.opacity = (Math.random()) / 10;
+    this.y = canvas.height + Math.random() * 100;
+    this.speed = Math.random() * 10;
+    this.radius = Math.random() * 20;
+    this.opacity = Math.random() / 10;
+}
+
+for (var i = 0; i < particleCount; i++) {
+    particles.push(new particle());
 }
 
 function loop() {
@@ -30,9 +43,13 @@ function draw() {
         ctx.arc(p.x, p.y, p.radius, 0, Math.PI * 2, false);
         ctx.fill();
         p.y -= p.speed;
-        if (p.y <= -10)
+        if (p.y <= -10) {
             particles[i] = new particle();
+        }
     }
 }
-loop();
 
+window.addEventListener('resize', resizeCanvas);
+
+resizeCanvas();
+loop();
